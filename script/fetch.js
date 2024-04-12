7eda39981b4ed4723f56d9d176721a4721b17833
Artem Kuznetsov <79201625618qwe@gmail.com>
2024-04-12 14:45:30 +0300

    init

---
+++ script/fetch.js
@@ -0,0 +1,20 @@
+
+export default class DataFetcher {
+    constructor(url) {
+      this.url = url;
+    }
+    async fetchData() {
+      try {
+        const response = await fetch(this.url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Fetch Error: ', error);
+        throw error;
+      }
+    }
+  }
+
--- script/index.js
+++ script/index.js
@@ -1,73 +1,73 @@
-// $(document).ready(function () {
-//   $(".cont-1_car-list-slider").slick({
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//     nextArrow:
-//       '<button type="button" class="slick-next"><img src="img/arrow.png" alt="next"></button>',
-//     prevArrow:
-//       '<button type="button" class="slick-prev"><img src="img/arrow.png" alt="prev"></button>',
-//     responsive: [
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//         },
-//       },
-//     ],
-//   });
-// });
+import DataFetcher from "./fetch.js";
+import DataPrinter from "./print.js";
 
-let news;
-fetch("json/news.json")
-  .then((response) => response.json()) // или .text() для текста
-  .then((data) => (news = data))
-  .then(()=>{
-    const domNews = document.querySelector("#news");
-    news.forEach((e) => {
-      let domItem = createItem();
-      let item = inputItem(domItem, e.date, e.text);
-      domNews.append(item);
-    });
-  })
-  .catch((error) => {
-    console.error("Ошибка:", error);
+$(document).ready(function () {
+  $(".cont-1_car-list-slider").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/arrow.png" alt="next"></button>',
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="img/arrow.png" alt="prev"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
-
-const createItem = () => {
-  let wrapper = document.createElement("div");
-  wrapper.className = "cont-2_news-item";
-  let domDate = document.createElement("div");
-  domDate.className = "div_date";
-  let domText = document.createElement("a");
-  domText.className = "div_txt";
-  domText.setAttribute("href", "#");
-
-  wrapper.append(domDate);
-  wrapper.append(domText);
-  return wrapper;
-};
-
-const inputItem = (wrapper, date, text) => {
-  wrapper.childNodes[0].innerHTML = date;
-  wrapper.childNodes[1].innerHTML = text;
-  return wrapper;
-};
+});
 
 window.onload = function () {
   const header = document.querySelector(".header");
-  const floatMenu = document.querySelector('#float-menu')
+  const floatMenu = document.querySelector("#float-menu");
   const headerHigh = header.clientHeight;
   let scrollPosition = 0;
   window.addEventListener("scroll", function () {
     scrollPosition = window.scrollY;
     if (headerHigh < scrollPosition) {
       floatMenu.style.opacity = 1;
-    } else{
+    } else {
       floatMenu.style.opacity = 0;
     }
   });
 
+  class DataProcessor {
+    constructor(dataFetcher) {
+      this.dataFetcher = dataFetcher;
+      this.data = null;
+    }
+    async setData() {
+      try {
+        this.data = await this.dataFetcher.fetchData();
+        return this.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+      }
+    }
+    printData() {
+      if (this.data) {
+        dataPrinter.print(this.data, domNews);
+      } else {
+        console.log("No data to print.");
+      }
+    }
+  }
+
+  const domNews = document.querySelector("#news");
+  const url = "json/news.json";
+  const dataFetcher = new DataFetcher(url);
+  const dataProcessor = new DataProcessor(dataFetcher);
+  const dataPrinter = new DataPrinter();
+
+  dataProcessor
+    .setData()
+    .then(() => dataProcessor.printData())
+    .catch((error) => console.error("Error:", error));
 };
