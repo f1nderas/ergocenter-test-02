e940be4cc582120d7d73a1558b61ac9a686b68ed
Artem Kuznetsov <79201625618qwe@gmail.com>
2024-05-02 14:47:03 +0300

    add passenger transportation

--- script/components/initStars.js
+++ script/components/initStars.js
@@ -1,33 +1,30 @@
 export default function initStars(stars) {
-  let starActive, starValue;
-  const initStarVars = (star) => {
-    starActive = star.querySelector(".stars_active");
-    starValue = document.querySelector(".reviews-rating_num");
-  };
-  const setStarActiveWidth = (i = starValue.innerHTML) => {
-    const starActiveWidth = i / 0.05;
+  const setStarActiveWidth = (star, value) => {
+    const starActive = star.querySelector(".stars_active");
+    const starActiveWidth = (value / 5) * 100; 
     starActive.style.width = `${starActiveWidth}%`;
   };
+
   const setStar = (star) => {
     const starItems = star.querySelectorAll(".stars_item");
     for (let i = 0; i < starItems.length; i++) {
       const starItem = starItems[i];
-      starItem.addEventListener("mouseenter", (e) => {
-        initStarVars(star);
-        setStarActiveWidth(starItem.value);
-      });
-      starItem.addEventListener("mouseleave", (e) => {
-        setStarActiveWidth();
+      starItem.addEventListener("click", (e) => {
+        if (star.classList.contains("stars_set")) {
+          const value = parseInt(starItem.value, 10);
+          star.setAttribute("data-rating", value); 
+          setStarActiveWidth(star, value); 
+        }
       });
     }
   };
+
   const initStar = (star) => {
-    initStarVars(star);
-    setStarActiveWidth();
-    if (star.classList.contains("stars_set")) {
-      setStar(star);
-    }
+    const starValue = parseFloat(star.getAttribute("data-rating")) || 0;
+    setStarActiveWidth(star, starValue); 
+    setStar(star); 
   };
+
   for (let i = 0; i < stars.length; i++) {
     const star = stars[i];
     initStar(star);
